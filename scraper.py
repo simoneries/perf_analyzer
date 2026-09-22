c1489ba1cb1c84c6ea48cb3ae3776962a537eeb3
simoneries <192712243+simoneries@users.noreply.github.com>
2026-09-22 20:13:23 +0200

    get account info

--- scraper.py
+++ scraper.py
@@ -3,9 +3,9 @@ import json
 
 cl = Client()
 
-cl.load_settings("session.json")
+cl.load_settings("sensitive/session.json")
 
-user = cl.user_info_by_username("xavierkmk")
+user = cl.user_info_by_username_v1("anoukeye")
 
 with open("instagram.json", "w") as f:
     json.dump(user.model_dump(), f, indent=2, default=str)
