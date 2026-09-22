0ff18eb303e589213762bcdc563173e77647f0eb
simoneries <192712243+simoneries@users.noreply.github.com>
2026-09-22 19:55:28 +0200

    Second commit

--- sensitive/auth.py
+++
@@ -1,6 +0,0 @@
-from instagrapi import Client
-
-cl = Client()
-cl.login_by_sessionid("23852741267%3AqeZwuaMe10ZxO9%3A3%3AAYmAtxc9a7RuhhrnRX-1SmxNVQnGXvTHtOs3G9873g")
-cl.dump_settings("session.json")
-print("session sauvegarde")
