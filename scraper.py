96b5baa2b972eec4f06c60d809a9785769200f78
simoneries <192712243+simoneries@users.noreply.github.com>
2026-09-23 19:45:48 +0200

    Stores media metadata in a df

--- scraper.py
+++ scraper.py
@@ -1,14 +1,36 @@
 from instagrapi import Client
 import json
+import pandas as pd
+import numpy as np
+import time
+import datetime
 
 cl = Client()
 
 cl.load_settings("sensitive/session.json")
 
-user = cl.user_info_by_username_v1("anoukeye")
+#get user infos for one account --> Get user infos for multiple accounts
+user = cl.user_info_by_username_v1("french.mush")
 
-with open("instagram.json", "w") as f:
-    json.dump(user.model_dump(), f, indent=2, default=str)
+
+#get user medias
+medias = cl.user_medias(user.pk, amount=3)
+
+#for m in medias: 
+    #print(m.code, m.like_count, m.comment_count)
+
+#store the posts data into a database
+
+#as json
+with open("medias.json","w") as f: 
+    for m in medias:
+        f.write(json.dumps(m.model_dump(mode="json"),ensure_ascii=False) + "\n")
+
+df = pd.read_json("/home/simon/repos/projects/portfolio/perf_analyzer/medias.json",lines=True)
+
+
+#daily updates of the database
+
 
 
 
