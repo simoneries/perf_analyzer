133411a46d505eefc688b887253dc2727da54655
simoneries <192712243+simoneries@users.noreply.github.com>
2026-09-24 17:40:10 +0200

    Get comments from pk

--- scraper.py
+++ scraper.py
@@ -11,39 +11,39 @@ cl.load_settings("sensitive/session.json")
 
 df_global = pd.DataFrame()
 
-def get_user_medias(username):
+def get_user_medias(username,comments_amount):
     #get user infos for one account --> Get user infos for multiple accounts
     user = cl.user_info_by_username_v1(username)
 
     #get user medias
-    medias = cl.user_medias(user.pk, amount=0)
+    medias = cl.user_medias(user.pk, amount=3)
+    rows = [m.model_dump(mode="json") for m in medias]
 
-    #for m in medias: 
-        #print(m.code, m.like_count, m.comment_count)
+    #put thee medias in a dataframe
+    df = pd.DataFrame(rows)
 
-    #store the posts data into a database
+    #set up the columns in the file
+    cols = ["user","pk","taken_at","media_type","crosspost","coauthor_producers","sponsor_tags","location","is_paid_partnership","is_affiliate","caption_text","like_count","comment_count","play_count"]
+    df_user = df[cols].copy()
+    df_user["pk"] = df["pk"].astype(str)
 
-    #as json
-    with open("medias.json","w") as f: 
-        for m in medias:
-            f.write(json.dumps(m.model_dump(mode="json"),ensure_ascii=False) + "\n")
-
-    df = pd.read_json("/home/simon/repos/projects/portfolio/perf_analyzer/medias.json",lines=True)
-
-    df_user = df[["user","pk","taken_at","media_type","crosspost","coauthor_producers","sponsor_tags","location","is_paid_partnership","is_affiliate","caption_text","like_count","comment_count","play_count"]].copy()
-
+    #get user comments 
+    df_user["comments"] = [
+        [c.model_dump(mode="json") for c in cl.media_comments(pk, amount=comments_amount)]
+        for pk in df_user["pk"]
+    ]
+    #unnest the user infos
     expanded_column = pd.json_normalize(df["user"])
-
     expanded_column = expanded_column.rename(columns={"pk":"pk_account"})
-
     df_expanded = pd.concat([expanded_column,df_user],axis=1)
 
+    #get clean df
     df_clean = df_expanded[['pk_account','username','is_verified','pk', 'taken_at', 'media_type', 'crosspost','coauthor_producers','sponsor_tags','location',
-        'is_paid_partnership', 'is_affiliate', 'caption_text', 'like_count', 'comment_count',
+        'is_paid_partnership', 'is_affiliate', 'caption_text', 'like_count', 'comment_count','comments',
         'play_count']].copy()
 
+    #get scraping date time
     scraping_datetime = datetime.datetime.now()
-
     df_clean["scraping_time"]=scraping_datetime
 
     return df_clean
@@ -53,7 +53,7 @@ users = ["french.mush","french.mush.it"]
 
 for user in users : 
     time.sleep(2) #for instagram bot detection
-    df_iteration = get_user_medias(user)
+    df_iteration = get_user_medias(user,30)
     df_global = pd.concat([df_global,df_iteration])
 
 df_global.to_csv("test.csv")
